a3d9bfd451599fff773bf012c6357d4ae8198e9c
Mintu Sikder <mintusikder15@gmail.com>
2024-03-29 09:22:37 +0600

    added logout form

--- src/components/LogOut/LogOut.jsx
+++ src/components/LogOut/LogOut.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const LogOut = () => {
+
+    const handelLogOut =(event) =>{
+        event.preventDefault()
+        const form = event.target  
+        const email = form.email.value 
+        const password = form.password.value 
+        const conform = form.conform.value
+
+        console.log(email, password, conform)
+    }
+
+
     return (
-        <div>
-            
-        </div>
+        <div className='form-container'>
+        <h2 className='form-title'>Register Please</h2>
+        <form onSubmit={handelLogOut}>
+            <div className="form-control">
+                <label htmlFor="email">Email</label>
+                <input placeholder='Your email' type="email" name="email" id="email" />
+            </div>
+            <div className="form-control">
+                <label htmlFor="password">Password</label>
+                <input placeholder='Your password' type="password" name="password" id="password" />
+            </div>
+            <div className="form-control">
+                <label htmlFor="conform">Conform Password</label>
+                <input placeholder='Your password' type="password" name="conform" id="password" />
+            </div>
+            <input className='btn-submit' type="submit" value="Register" />
+        </form>
+        <p><small>Already have an account? <Link to='/login'>Login</Link></small></p>
+    </div>
     );
 };
 
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import './Login.css'
+import { Link } from 'react-router-dom';
 const Login = () => {
     return (
-        <div>
-            <h2>Login coming sooon</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login Please</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input placeholder='Your email' type="email" name="email" id="email" />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input placeholder='Your password' type="password" name="password" id="password" />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <p><small>New to Ema-john? <Link to='/logout'>Create New Account</Link></small> </p>
         </div>
     );
 };
