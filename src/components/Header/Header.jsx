9537cbac2fc6b579657eb0178bd30c5c90cddffe
Mintu Sikder <mintusikder15@gmail.com>
2024-03-29 10:23:55 +0600

    added auth Provider

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,27 @@
-import React from 'react';
-import './Header.css';
-import logo from '../../images/Logo.svg';
-import { Link } from 'react-router-dom';
+import React, { useContext } from "react";
+import "./Header.css";
+import logo from "../../images/Logo.svg";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../provider/AuthProvider";
+
 
 const Header = () => {
-    return (
-        <nav className='header'>
-            <img src={logo} alt="" />
-            <div>
-                <Link to="/">Shop</Link>
-                <Link to="/orders">Orders</Link>
-                <Link to="/inventory">Inventory</Link>
-                <Link to="/login">Login</Link>
-                <Link to="/logout">Sign Up</Link>
-            </div>
-        </nav>
-    );
+  const { user } = useContext(AuthContext);
+console.log(user)
+
+  return (
+    <nav className="header">
+      <img src={logo} alt="" />
+      <div>
+        <Link to="/">Shop</Link>
+        <Link to="/orders">Orders</Link>
+        <Link to="/inventory">Inventory</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/logout">Sign Up</Link>
+        <p>{user && <span>{user.email}</span>}</p>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import Login from './components/Login/Login';
 import cartProductsLoader from './loaders/cartProductsLoader';
 import Checkout from './components/Checkout/Checkout';
 import LogOut from './components/LogOut/LogOut';
+import AuthProvider from './provider/AuthProvider';
 
 const router = createBrowserRouter([
   {
@@ -51,6 +52,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+   <AuthProvider>
+   <RouterProvider router={router} />
+   </AuthProvider>
   </React.StrictMode>,
 )
